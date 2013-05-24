f7e5d2dc8035daba7513a9ac186cd4df235df43f
croniccorey <cronmod.dev@gmail.com>
2013-05-23 23:49:35 -0400

    MSM: Fix compile of socinfo.c

--- arch/arm/mach-msm/socinfo.c
+++ arch/arm/mach-msm/socinfo.c
@@ -589,6 +589,10 @@ arch_initcall(socinfo_init_sysdev);
 
 void *setup_dummy_socinfo(void)
 {
+#ifdef CONFIG_ARCH_QSD8X50
+	dummy_socinfo.id = 30; // look above: [30] = MSM_CPU_8X50,
+	return (void *) &dummy_socinfo;
+#else
 	if (machine_is_msm8960_rumi3() || machine_is_msm8960_sim() ||
 	    machine_is_msm8960_cdp())
 		dummy_socinfo.id = 87;
@@ -599,6 +603,7 @@ void *setup_dummy_socinfo(void)
 	else if (early_machine_is_copper())
 		dummy_socinfo.id = 126;
 	return (void *) &dummy_socinfo;
+#endif
 }
 
 int __init socinfo_init(void)
