3e0188cc7a5450d9c63e0c4ae8ed96e4dced3750
croniccorey <cronmod.dev@gmail.com>
2013-05-24 05:22:47 -0400

    MSM: Fix compile of board-bravo.c

--- arch/arm/mach-msm/board-bravo.c
+++ arch/arm/mach-msm/board-bravo.c
@@ -24,6 +24,7 @@
 #include <linux/input.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/usb/android_composite.h>
 #include <linux/usb/f_accessory.h>
