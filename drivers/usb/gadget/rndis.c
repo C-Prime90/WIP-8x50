2c5897730e6b925d7daff1aa389d57f3ffd513f6
croniccorey <cronmod.dev@gmail.com>
2013-06-17 16:35:32 -0400

    DRIVERS: Fix commit 0bc9fb34c1067a2d43c5af22ddb7a3c0ead1a5c6

--- drivers/usb/gadget/rndis.c
+++ drivers/usb/gadget/rndis.c
@@ -159,12 +159,15 @@ static const u32 oid_supported_list[] =
 #endif	/* RNDIS_PM */
 };
 
-/*
+/* Disabled as of 3.4.39 (defined elseware)
+ *
  * HACK: copied from net/core/dev.c to replace dev_get_stats since
  * dev_get_stats cannot be called from atomic context
+ *
  */
 
-void netdev_stats_to_stats64(struct rtnl_link_stats64 *stats64,
+#if 0
+static void netdev_stats_to_stats64(struct rtnl_link_stats64 *stats64,
 				    const struct net_device_stats *netdev_stats)
 {
 #if BITS_PER_LONG == 64
@@ -181,6 +184,7 @@ void netdev_stats_to_stats64(struct rtnl_link_stats64 *stats64,
 		dst[i] = src[i];
 #endif
 }
+#endif
 
 /* NDIS Functions */
 static int gen_ndis_query_resp(int configNr, u32 OID, u8 *buf,
